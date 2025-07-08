61203931e784d43a4b28243cb28f60a602adbaa4
Jasur <jasurhamroqulov260@gmail.com>
2025-07-08 18:22:36 +0500

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        Arrays.sort(numbers);
+        int minsum = numbers[0] + numbers[1] + numbers[2] + numbers[3];
+        int maxsum = numbers[1] + numbers[2] + numbers[3] + numbers[4];
+        System.out.println(minsum + " " + maxsum);
     }
 }
