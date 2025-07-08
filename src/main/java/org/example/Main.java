b8081c18f85612d82caabe15c719a095ad3f6b3c
Jasur <jasurhamroqulov260@gmail.com>
2025-07-08 18:26:34 +0500

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,9 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[5];
-        for (int i = 0; i < 5; i++) {
-            numbers[i] = scanner.nextInt();
+        for (int i = 1; i < 20; i += 2) {
+            System.out.print(i + ", ");
+
         }
-        Arrays.sort(numbers);
-        int minsum = numbers[0] + numbers[1] + numbers[2] + numbers[3];
-        int maxsum = numbers[1] + numbers[2] + numbers[3] + numbers[4];
-        System.out.println(minsum + " " + maxsum);
     }
 }
